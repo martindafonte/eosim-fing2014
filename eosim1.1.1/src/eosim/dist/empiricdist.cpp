a4719e99e5f5843585c06a7e1a520b4c3ba28ff0
el.dela89 <el.dela89@gmail.com>
2014-10-11 16:14:49 +0000

    terminado e 8

--- eosim1.1.1/src/eosim/dist/empiricdist.cpp
+++ eosim1.1.1/src/eosim/dist/empiricdist.cpp
@@ -11,6 +11,16 @@ EmpiricDist::EmpiricDist(GenType gen, double* f_x, int size):
 EmpiricDist::~EmpiricDist() {}
 
 double EmpiricDist::sample() {
+	if (size > 1){
+		double u = this->generator->nextDouble();
+		int x = 1;
+
+		while (u > f_x[x]){
+			x++;
+		}
+
+		return x - 1 + ((u - f_x[x-1])/(f_x[x] - f_x[x-1]));
+	}
 
     return 0.0;
 }
