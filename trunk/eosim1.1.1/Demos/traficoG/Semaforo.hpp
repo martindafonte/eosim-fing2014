432a89fdc37b6685bae0edee3bb79035b22d29e3
martindafonte@gmail.com <martindafonte@gmail.com@77e89be3-2b59-9c1f-c75b-832cd95d9acc>
2014-11-15 20:24:38 +0000

    TrafficG compilado

--- trunk/eosim1.1.1/Demos/traficoG/Semaforo.hpp
+++ trunk/eosim1.1.1/Demos/traficoG/Semaforo.hpp
@@ -2,12 +2,14 @@
 #define Semaforo_HPP_
 
 #include <eosim/core/entity.hpp>
-
+#include <eosim/graphic/display.hpp>
 
 class Semaforo: public eosim::core::Entity{
 public:
+	eosim::graphic::Sprite spr_l;
+	eosim::graphic::Sprite spr_r;
 	bool verde;
-	Semaforo(){}
+	Semaforo(eosim::graphic::Display& disp, double posx_l, double posy_l, double posx_r, double posy_r) : spr_l(disp,"../Images/roja.PNG", posx_l, posy_l),spr_r(disp,"../Images/roja.PNG", posx_r, posy_r)  {}
 	~Semaforo() {}
 };
 
--- trunk/eosim1.1.1/Demos/traficoG/traffic.cpp
+++ trunk/eosim1.1.1/Demos/traficoG/traffic.cpp
@@ -14,6 +14,10 @@ Traffic::Traffic(double p_media_vehiculos, double p_media_izq, double p_media_de
 	eIniCruzarCC(*this),
 	eIniCruzarCI(*this),
 	eIniCruzarCD(*this),
+	disp(1675,720),semaforo_l(580,460),semaforo_r(580,280),tamaño_auto(108,53),
+	inicio_di(0,0),inicio_ci(0,0),inicio_cc(0,0),inicio_cd(0,0),inicio_dd(0,0),
+	fin_di(0,0),fin_ci(0,0),fin_cc(0,0),fin_cd(0,0),fin_dd(0,0),
+	rG(*this,disp),
 	dis_llegada(MT19937,(50/p_media_vehiculos)),//Se puse 50 para permitir variar los tiempos de luz
 	dis_carriles(MT19937,0,3),
 	dis_direccion(MT19937,1,1000),
@@ -38,7 +42,8 @@ Traffic::Traffic(double p_media_vehiculos, double p_media_izq, double p_media_de
 	lCola_cd("Largo Cola CD",*this),
 	lCola_ci("Largo Cola CI",*this)
 {
-	semaforo = new Semaforo();
+	disp.setSpeed(0.02);
+	semaforo = new Semaforo(disp,semaforo_l.first,semaforo_l.second,semaforo_r.first,semaforo_r.second);
 }
 
 	Traffic::~Traffic() {}
@@ -55,6 +60,7 @@ void Traffic::init() {
 	registerCEvent(&eIniCruzarCD);
 	registerCEvent(&eIniCruzarDI);
 	registerCEvent(&eIniCruzarDD);
+	registerCEvent(&rG);
 	
 	registerDist(&dis_carriles);
 	registerDist(&dis_llegada);
@@ -63,4 +69,5 @@ void Traffic::init() {
 void Traffic::doInitialSchedules() {
 	schedule(0.0, semaforo, luz_verde);
 	schedule(0.0,new Vehiculo(),llega_v);
+	disp.setBackground("../Images/Fondo.jpg");
 }
